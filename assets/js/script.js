c885a50af7cc55fbf39c5814a14cd3d0b635bf39
anagha778 <anagha778@gmail.com>
2020-12-16 15:50:36 -0500

    function to accept character type added

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,34 @@ var acceptLength = function()
   return promptLength;
 };
 
+
+//Function to accept Character type from user and validate it 
+var acceptcharType = function()
+{ //Provide choice to user to select character type
+  var promptcharType = window.prompt("Please enter type of characters, select from following choices (1 or 2):\n"+
+                                      "1 : lowercase,uppercase characters and numbers\n"+
+                                      "2 : lowercase,uppercase characters, numbers and special characters ");
+  while (promptcharType===null)//If user select Cancel, come out of program
+  { 
+   return 1;
+  }
+
+  switch (parseInt(promptcharType)){
+    case 1:
+      var promptPassStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      return promptPassStr;
+      break;
+    case 2:
+      var promptPassStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+      return promptPassStr;
+      break;
+    default:
+      window.alert('You did not pick a valid option. Try again.');
+      return 0;
+      break;
+  }  
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
