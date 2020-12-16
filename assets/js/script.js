83e22826e4ecf011168e40606491f4440053539a
anagha778 <anagha778@gmail.com>
2020-12-16 15:49:23 -0500

    function to accept length added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
 
+//Function to accept length from user and validate it
+
+var acceptLength = function()
+{ //Provide prompt to user to enter password length
+  var promptLength = window.prompt("Please enter length of the password, length can be between 8 to 128 characters");
+  
+  while(promptLength===null)//If user select Cancel, come out of program
+  { return 1;
+  }
+  
+  if (parseInt(promptLength) < 8 || parseInt(promptLength) > 128 || promptLength ==="")
+  {
+    window.alert("Please enter number between 8 to 128.");
+    return 0;
+  }
+
+  return promptLength;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
