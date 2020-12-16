7dcc1a0ce3b023e145f4cd4df26ea8358b622f04
anagha778 <anagha778@gmail.com>
2020-12-16 15:54:28 -0500

    updated writePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,11 +81,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+  if (password !="")
+  {
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+  }
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
